2e3aeb832de25c846e9813b3f537e550eef6ec51
Emmett-Kogan <emmett.kogan@gmail.com>
2023-11-04 17:47:12 -0400

    Measuring voltages using an ADC done, now I need to hook up the IR receiver, and print that componenet's output using the ADC to the console, point a remote at it and see what happens.

--- remote/src/main.cpp
+++ remote/src/main.cpp
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+// Goal:
+// Sample a pin at an interval and print the result to the terminal
+
+#define INTERVAL 1000
+#define CONVERT (3.3f / (1<<12))
 
 int main() {
     // Init peripherals
     stdio_init_all();
+    adc_init();
+
+    // init ADC pin
+    adc_gpio_init(26);  // silkscreen label A0
+    adc_select_input(0);
 
     while (1) {
+
+        uint16_t temp = adc_read();
+
         printf("\033[1;1H\033[2J");
-        printf("Hello, World!\n");
-        sleep_ms(500);
+        printf("ADC raw val %d\n", temp);
+        printf("ADC converted val %f V\n", temp*CONVERT);
+        sleep_ms(INTERVAL);
     }
       
 }
