f1132ec091ee752819fb33d93eb78a0444d81fce
Matthew <matthew.darrow3@gmail.com>
2024-04-03 20:32:42 -0400

    added 10 second running average prediction result to mitigate false positives

--- tv-commerical detection/main.py
+++ tv-commerical detection/main.py
@@ -3,11 +3,15 @@ import numpy as np
 import subprocess
 import time
 import threading
+import RPi.GPIO as GPIO
+from collections import deque
 from keras.models import load_model
 from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 
 label = None
 prediction = None
+average = None
+prediction_results = deque(maxlen=10)
 
 model = load_model('commercial_detection_model.h5')
 
@@ -20,6 +24,13 @@ def execute_command(command):
         return False
     return True
 
+def average_prediction(predictions):
+    total = 0.0
+    for prediction in predictions:
+        total += prediction
+    num_predictions = len(predictions)
+    return (total / num_predictions)
+
 def preprocess_frame(frame):
     img = cv2.resize(frame, (224, 224))
     return img
@@ -27,15 +38,20 @@ def preprocess_frame(frame):
 def predict_commercial(frame):
     global label
     global prediction
+    global average
         
     processed_frame = preprocess_frame(frame)
 
     prediction = model.predict(np.expand_dims(processed_frame, axis=0))[0, 0]
-    print(prediction)
+    prediction_results.append(prediction)
+    average = average_prediction(prediction_results)
 
     threshold = 0.5
     label = "Non-commercial" if prediction < threshold else "Commercial"
     prediction = f"Predicted: {prediction}"
+
+def detect_commercial_transition():
+    return (average > 0.5)
 
 def main():
     execute_command('v4l2-ctl --set-edid=file=edid.txt --fix-edid-checksums')
@@ -43,6 +59,9 @@ def main():
     execute_command('v4l2-ctl -V')
     execute_command('v4l2-ctl --set-dv-bt-timings query')
     execute_command('v4l2-ctl -v pixelformat=UYVY')
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(14, GPIO.OUT)
 
     cap = cv2.VideoCapture("/dev/video0")
 
@@ -62,7 +81,16 @@ def main():
 
         if prediction is not None:
             cv2.putText(frame, prediction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            cv2.putText(frame, label, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.putText(frame, f"Avg: {average}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(frame, label, (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            if len(prediction_results) == prediction_results.maxlen and detect_commercial_transition():
+                cv2.rectangle(frame, (300, 200), (frame.shape[1], 200), (214, 208, 34), -1)
+                cv2.putText(frame, "Transition to TV commercial detected!", (770, 65), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
+                GPIO.output(14, GPIO.HIGH)
+                print(f"GPIO pin 14 asserted (set to HIGH).")
+            else:
+                GPIO.output(14, GPIO.LOW)
+                print(f"GPIO pin 14 reset (set to LOW).")  
         else:
             cv2.putText(frame, "Predicting...", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         
@@ -78,6 +106,7 @@ def main():
             break
 
     cap.release()
+    GPIO.cleanup()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
